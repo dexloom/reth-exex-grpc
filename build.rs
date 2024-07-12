0f67b79f03c35e91610804913debb96f670cd8d4
dexloom <xdexloom@gmail.com>
2024-07-12 07:21:10 +0200

    --experimental_allow_proto3_optional

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() {
-    tonic_build::compile_protos("proto/exex.proto --experimental_allow_proto3_optional")
+    tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
+        .compile(&["proto/exex.proto"], &["proto"] )
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 }
